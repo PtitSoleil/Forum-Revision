2f45beb4a6fae52eda38c7c2bbd55328fdf60e49
PtitSoleil <adar-sores.gnr@eduge.ch>
2018-08-30 16:18:05 +0200

    ajout fichier du chapitre 3

---
+++ Chapitre_3/func.php
@@ -0,0 +1,91 @@
+<?php 
+
+/*
+  Auteur : Guner, Adar, I.DA-P2B
+  Projet : Quiz !
+  Fichier : Quiz !
+  Description : Jeu de quiz avec plusieurs questions de différentes catégories
+  Version : 06.06.2018
+ */
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+function isLogged() {
+    return isset($_SESSION['connect']);
+}
+
+//Initialisation
+$surnameR = filter_input(INPUT_POST, 'surnameR', FILTER_SANITIZE_STRING);
+$nameR = filter_input(INPUT_POST, 'nameR', FILTER_SANITIZE_STRING);
+$usernameR = filter_input(INPUT_POST, 'usernameR', FILTER_SANITIZE_STRING);
+$pwdR = filter_input(INPUT_POST, 'pwdR', FILTER_SANITIZE_STRING);
+$confirmPwdR = filter_input(INPUT_POST, 'confirmPwdR', FILTER_SANITIZE_STRING);
+
+
+$usernameL = filter_input(INPUT_POST, "usernameL");
+$pwdL = filter_input(INPUT_POST, "pwdL");
+
+
+$msgError = "";
+        
+$errors = array();
+
+function inscription($surnameR, $nameR, $usernameR, $pwdR) {
+    $db = new PDO('mysql:host=localhost;charset=utf8;dbname=a-distribuer', 'root');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    try {
+        $db->query('INSERT INTO tbl_user (Txt_surname,Txt_name,Txt_login,Txt_password) VALUES ("' . $surnameR . '","' . $nameR . '","' . $usernameR . '","' . $pwdR . '")');
+        header("Location: index.php");
+    } catch (PDOException $ex) {
+        echo "An Error occured!"; // user friendly message
+        error_log($ex->getMessage());
+    }
+}
+
+if (filter_has_var(INPUT_POST, 'register')) {
+    if (empty($surnameR)) {
+        $errors["surnameR"] = "Le prénom n'est pas valide";
+    }
+    if (empty($nameR)) {
+        $errors["nameR"] = "Le nom n'est pas valide";
+    }
+    if (empty($usernameR)) {
+        $errors["usernameR"] = "L'identifiant n'est pas valide";
+    }
+    if ($pwdR != $confirmPwdR) {
+        $errors["pwdR"] = "Le mot de passe n'est pas identique";
+        $errors["confrimPwdR"] = "Le mot de passe n'est pas identique";
+    }
+
+    if (empty($errors)) {
+        inscription($surnameR, $nameR, $usernameR, $pwdR);
+    }
+}
+
+function connexion($usernameL, $pwdL) {
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=a-distribuer', 'root');
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+    $requeteUser = $db->prepare("SELECT * FROM tbl_user WHERE Txt_login = ? and Txt_password = ?");
+    $requeteUser->execute(array($usernameL, $pwdL));
+    $user = $requeteUser->fetch();
+    $utilisateurExist = $requeteUser->rowCount();
+    if ($utilisateurExist == 1) {
+        $_SESSION["connect"] = true;
+        $_SESSION['idUser'] = $user['idUser'];
+        header("Location: main.php");
+    } else {
+        $msgError = "Le pseudo ou le mot de passe est faux !";
+    }
+}
+
+//Traitement
+if ($usernameL && $pwdL) {
+    connexion($usernameL, $pwdL);
+}
---
+++ Chapitre_3/register.php
@@ -0,0 +1,56 @@
+<?php
+/*
+  Auteur : Guner, Adar, I.DA-P3B
+  Projet : A Distribuer
+  Fichier : A Distribuer
+  Description : Se connecter puis ajouter des news
+  Version : 30.08.2018
+ */
+
+require_once './func.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form method='post' action="">
+            <fieldset>
+                <legend>Connection</legend>
+                <table class="tableauInscription">
+                    <tr>
+                        <td class="info">Prenom:</td>
+                        <td class="info"><input type="text" name="surnameR" value=""></td>
+                        <td class="error"><?php if (isset($errors['surnameR'])) { echo $errors['surnameR']; } ?></td>
+                    </tr>
+                    <tr>
+                        <td class="info">Nom:</td>
+                        <td class="info"><input type="text" name="nameR" value=""></td>
+                        <td class="error"><?php if (isset($errors['nameR'])) { echo $errors['nameR']; } ?></td>
+                    </tr>
+                    <tr>
+                        <td class="info">Identifiant:</td>
+                        <td class="info"><input type="text" name="usernameR" value=""></td>
+                            <td class="error"><?php if (isset($errors['usernameR'])) { echo $errors['usernameR']; } ?></td>
+                    </tr>
+                    <tr>
+                        <td class="info">Mot de passe:</td>
+                        <td class="info"><input type="password" name="pwdR" value=""></td>
+                        <td class="error"><?php if (isset($errors['pwdR'])) { echo $errors['pwdR']; } ?></td>
+                    </tr>
+                    <tr>
+                        <td class="info">Validation du mot de passe:</td>
+                        <td class="info"><input type="password" name="confirmPwdR" value=""></td>
+                        <td class="error"><?php if (isset($errors['confrimPwdR'])) { echo $errors['confrimPwdR']; } ?></td>
+                    </tr>
+                </table>
+                <input type="submit" name="register" value="Valider">
+            </fieldset>
+        </form>
+        <nav>
+            <a href="./index.php">Retourn sur connection</a>
+        </nav>
+    </body>
+</html>
