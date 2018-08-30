f733224a07afed5bde737d345b22cdf89456a4e9
PtitSoleil <adar-sores.gnr@eduge.ch>
2018-08-30 14:49:08 +0200

    sauvegarde deu chapitre 2 (1)

--- Chapitre_2/func.php
+++ Chapitre_2/func.php
@@ -8,13 +8,27 @@
   Version : 06.06.2018
  */
 
-//Entrees par l'utilisateur
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+function isLogged() {
+    return isset($_SESSION['connect']);
+}
+
+//Initialisation
 $surnameR = filter_input(INPUT_POST, 'surnameR', FILTER_SANITIZE_STRING);
 $nameR = filter_input(INPUT_POST, 'nameR', FILTER_SANITIZE_STRING);
 $usernameR = filter_input(INPUT_POST, 'usernameR', FILTER_SANITIZE_STRING);
 $pwdR = filter_input(INPUT_POST, 'pwdR', FILTER_SANITIZE_STRING);
 $confirmPwdR = filter_input(INPUT_POST, 'confirmPwdR', FILTER_SANITIZE_STRING);
 
+
+$usernameL = filter_input(INPUT_POST, "usernameL");
+$pwdL = filter_input(INPUT_POST, "pwdL");
+
+$msgError = "";
+        
 $errors = array();
 
 function inscription($surnameR, $nameR, $usernameR, $pwdR) {
@@ -45,9 +59,30 @@ if (filter_has_var(INPUT_POST, 'register')) {
         $errors["pwdR"] = "Le mot de passe n'est pas identique";
         $errors["confrimPwdR"] = "Le mot de passe n'est pas identique";
     }
-    
 
     if (empty($errors)) {
         inscription($surnameR, $nameR, $usernameR, $pwdR);
     }
+}
+
+function connexion($usernameL, $pwdL) {
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=a-distribuer', 'root');
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+    $requeteUser = $db->prepare("SELECT * FROM tbl_user WHERE Txt_login = ? and Txt_password = ?");
+    $requeteUser->execute(array($usernameL, $pwdL));
+    $utilisateurExist = $requeteUser->rowCount();
+    if ($utilisateurExist == 1) {
+        $_SESSION["connect"] = true;
+        header("Location: confirmation.php");
+    } else {
+        $msgError = "Le pseudo ou le mot de passe est faux !";
+    }
+}
+
+//Traitement
+if ($usernameL && $pwdL) {
+    connexion($usernameL, $pwdL);
 }
