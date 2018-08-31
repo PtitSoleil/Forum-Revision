2300e13d9238703f05ae6ff6f1c1f901de72286f
PtitSoleil <adar-sores.gnr@eduge.ch>
2018-08-31 09:21:15 +0200

    Finition du chapitre 3 et commencement du chapitre 4

---
+++ Chapitre_4/confirmation.php
@@ -0,0 +1,32 @@
+<?php
+/*
+  Auteur : Guner, Adar, I.DA-P3B
+  Projet : A Distribuer
+  Description : Se connecter puis ajouter des news
+  Version : 30.08.2018
+ */
+
+require_once './func.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Vous êtes connecté !</title>
+    </head>
+    <body>
+        <?php
+        $db = new PDO('mysql:host=localhost;dbname=a-distribuer', 'root');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        try {
+            foreach ($db->query('SELECT * FROM tbl_user WHERE idUser = "'.$_SESSION['idUser'].'"') as $row) {
+                echo "Bonjour ".$row['Txt_surname'] . " " . $row['Txt_name']. ", vous êtes connecté";
+            }
+        } catch (PDOException $ex) {
+            echo 'An Error occured!'; // user friendly message
+            error_log($ex->getMessage());
+        }
+        ?>
+    </body>
+</html>
